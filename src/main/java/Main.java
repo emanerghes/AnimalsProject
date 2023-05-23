d49017f727f15e09e85c2e7cf14acec465edd2cf
Emanuela Nerghes <eanerghes@gmail.com>
2023-05-23 20:44:27 +0300

    Main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
-public class Main {
     public class Main {
         public static void main(String[] args) {
             // Create instances of different animals
-            Dog dog = new Dog("Bobby", "Woof", "Labrador");
-            Cat cat = new Cat("Whiskers", "Meow", "Tabby");
-            Mouse mouse = new Mouse("Jerry", "Squeak", 1, true);
+            Animal dog = new Dog("Bobby", "Woof", "Labrador");
+            Animal cat = new Cat("Whiskers", "Meow", "Tabby");
+            Animal mouse = new Mouse("Jerry", "Squeak", 1, true);
 
+            dog.makeSound();
+            cat.makeSound();
+            mouse.makeSound();
         }
     }
-}
