b7b5940399b24a60a4d5880cd151aca924c9a67e
Emanuela Nerghes <eanerghes@gmail.com>
2023-05-23 20:30:55 +0300

    Main class created

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public class Main {
+        public static void main(String[] args) {
+            // Create instances of different animals
+            Dog dog = new Dog("Bobby", "Woof", "Labrador");
+            Cat cat = new Cat("Whiskers", "Meow", "Tabby");
+            Mouse mouse = new Mouse("Jerry", "Squeak", 1, true);
+
+        }
+    }
+}
